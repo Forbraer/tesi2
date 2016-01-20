0f2a3c285c003b1f89b96e172fc653ab6f78f845
Forbraer <fortunatoneto@outlook.com>
2016-01-20 10:51:29 -0500

    Presença
    Presença de um aluno adicionada.

--- Calculadora.java
+++ Calculadora.java
@@ -2,5 +2,7 @@ public Class Calculadora{
   public static void main(String [] args){
   int a,b;
   System.out.println(a+b);
+  
+  System.out.println("Aluno 20120300012? PRESENTE professor!");
   }
 }  
